a7a3f1c1d86abef275b1c5c700bd71223a9389a2
Silvio Meneguzzo <meneguzzosilvio@gmail.com>
2023-09-26 22:12:45 +0200

    update versions and description

--- src/components/About.js
+++ src/components/About.js
@@ -5,9 +5,9 @@ const About = () => {
         <section className='about'>
             <h2>About Me</h2>
             <p>
-                I am PhD in Computer Science and Blockchain developer, with wide
-                transversal skills adaptable to the working
-                and academic world.
+            PhD in Computer Science and Blockchain Developer with a proven ability to apply research to real-world problems. 
+            Expertise in distributed systems, cryptography, and smart contracts. 
+            Passionate about using technology to solve complex problems and make a positive impact on the world.
 
             </p>
         </section>
